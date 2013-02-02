616f653dec1bef265199cb0239a776bea3f2ca8a
Siddhartha Gudipati <sid@web2labs.net>
2013-02-02 12:34:58 -0500

    Updated the styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+(function(){
 
+
+	var offset = 50;
+
+$('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
+})();
